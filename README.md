a1d3546b871aaa4af33b30d1e52c768ad50ac7bf
ak-dvlp <Aleksandr-Karpenko-86@yandex.ru>
2026-08-25 20:59:25 +0300

    откорректирован скрипт etl_to_clickhouse.py; отредактирован файл README.md

--- README.md
+++ README.md
@@ -2,15 +2,49 @@
 
 #### Инженерный проект по построению высокопроизводительного аналитического конвейера данных на базе ClickHouse и Apache Superset.
 
-Основная цель — обеспечить эффективную загрузку, хранение и визуализацию больших объёмов данных (более 16 млн строк).
+Основная цель — обеспечить эффективную загрузку, хранение и визуализацию больших объёмов данных (более 13 млн строк).
 
 #### 🗺️ Конвейер данных:
 
 Parquet-файлы ➡️ ClickHouse (OLAP) ➡️ Apache Superset (Визуализация данных).
 
-
 #### Запуск ETL-скрипта в изолированном окружении Docker
 
 ```bash
 docker compose exec superset /app/.venv/bin/python /app/etl_to_clickhouse.py
+```
+
+#### Запуск ETL-скрипта локально
+
+```bash
+poetry run python scripts/etl_to_clickhouse.py
+```
+
+#### Подключение ClickHouse к Apache Superset
+
+```bash
+clickhouse+connect://bi_user:bi_password@127.0.0.1:8124/ecommerce_olap
+```
+
+```sql
+SELECT
+    order_id,
+    order_date,
+    order_total_amount,
+    orderline_id,
+    quantity,
+    unit_price,
+    subtotal,
+    product_name
+FROM ecommerce_olap.superset_flat_analytics
+WHERE order_id IN (
+    SELECT order_id
+    FROM ecommerce_olap.superset_flat_analytics
+    GROUP BY order_id
+    HAVING count() > 2
+    LIMIT 10
+)
+ORDER BY order_id ASC, orderline_id ASC
+LIMIT 100;
+
 ```
--- scripts/etl_to_clickhouse.py
+++ scripts/etl_to_clickhouse.py
@@ -1,23 +1,25 @@
 import time
-import clickhouse_connect # type: ignore
+import clickhouse_connect  # type: ignore
 
 # Настройки подключения
 CH_HOST = '127.0.0.1'
-CH_PORT = 9001  
+CH_PORT = 8124  # Используем внешний HTTP-порт для clickhouse-connect
 CH_USER = 'bi_user'
 CH_PASSWORD = 'bi_password'
 CH_DATABASE = 'ecommerce_olap'
 
+
 def run_etl():
     print("⏳ Подключение к ClickHouse...")
     client = clickhouse_connect.get_client(
-        host=CH_HOST, 
-        port=CH_PORT, 
-        username=CH_USER, 
-        password=CH_PASSWORD
+        host=CH_HOST, port=CH_PORT, username=CH_USER, password=CH_PASSWORD
     )
 
+    # Создание базы данных, если её нет
     client.command(f"CREATE DATABASE IF NOT EXISTS {CH_DATABASE}")
+
+    # Удаляем старую аналитическую таблицу, если она осталась от прошлых попыток
+    client.command(f"DROP TABLE IF EXISTS {CH_DATABASE}.superset_flat_analytics")
 
     # SQL для создания плоской таблицы
     create_table_ddl = f"""
@@ -63,7 +65,29 @@ def run_etl():
 
     print("⏳ Создание плоской таблицы superset_flat_analytics...")
     client.command(create_table_ddl)
-    print("✅ Таблица успешно создана (или уже существовала).")
+    print("✅ Таблица superset_flat_analytics успешно создана.")
+
+    print("⏳ Создание быстрых временных таблиц в памяти...")
+    client.command(
+        f"DROP TABLE IF EXISTS {CH_DATABASE}.tmp_products"
+    )  # Защита от дублей
+    client.command(
+        f"DROP TABLE IF EXISTS {CH_DATABASE}.tmp_customers"
+    )  # Защита от дублей
+    client.command(
+        f"DROP TABLE IF EXISTS {CH_DATABASE}.tmp_orders"
+    )  # Защита от дублей
+
+    client.command(
+        f"CREATE TABLE {CH_DATABASE}.tmp_products ENGINE = Memory AS SELECT * FROM file('parquet/product_part1.parquet', Parquet)"
+    )
+    client.command(
+        f"CREATE TABLE {CH_DATABASE}.tmp_customers ENGINE = Memory AS SELECT * FROM file('parquet/customer_part1.parquet', Parquet)"
+    )
+    client.command(
+        f"CREATE TABLE {CH_DATABASE}.tmp_orders ENGINE = Memory AS SELECT * FROM file('parquet/order_part*.parquet', Parquet)"
+    )
+    print("✅ Временные таблицы успешно созданы.")
 
     # SQL для вставки данных
     insert_dml = f"""
@@ -76,28 +100,40 @@ def run_etl():
         p.category, p.subcategory, p.brand, p.price AS product_price, p.cost AS product_cost,
         p.stock_quantity, p.weight_kg, p.status AS product_status, p.rating_average, p.review_count,
         c.customer_id, c.firstname, c.lastname, c.age, c.postalcode, c.city
-    FROM file('orderline_part*.parquet', Parquet) AS ol
-    GLOBAL INNER JOIN file('order_part*.parquet', Parquet) AS o ON ol.order_id = o.order_id
-    GLOBAL INNER JOIN file('product_part1.parquet', Parquet) AS p ON ol.product_id = p.product_id
-    GLOBAL INNER JOIN file('customer_part1.parquet', Parquet) AS c ON o.customer_id = c.customer_id
+    FROM (
+        SELECT * FROM file('parquet/orderline_part*.parquet', Parquet)
+    ) AS ol
+    INNER JOIN {CH_DATABASE}.tmp_orders AS o ON ol.order_id = o.order_id
+    INNER JOIN {CH_DATABASE}.tmp_products AS p ON ol.product_id = p.product_id
+    INNER JOIN {CH_DATABASE}.tmp_customers AS c ON o.customer_id = c.customer_id
     SETTINGS max_memory_usage = 10000000000;
     """
 
     print("⏳ Запуск денормализации и импорта строк из Parquet...")
     start_time = time.time()
-    
+
     client.command(insert_dml)
-    
+
     end_time = time.time()
     duration = end_time - start_time
     print(f"✅ Импорт завершён. Время выполнения: {duration:.2f} сек.")
 
+    # Очистка ресурсов оперативной памяти ClickHouse
+    print("⏳ Удаление временных таблиц...")
+    client.command(f"DROP TABLE IF EXISTS {CH_DATABASE}.tmp_products")
+    client.command(f"DROP TABLE IF EXISTS {CH_DATABASE}.tmp_customers")
+    client.command(f"DROP TABLE IF EXISTS {CH_DATABASE}.tmp_orders")
+    print("✅ Временные таблицы удалены.")
+
     # Проверка результата
     print("⏳ Проверка загруженных данных...")
-    count_res = client.command(f"SELECT count() FROM {CH_DATABASE}.superset_flat_analytics")
+    count_res = client.command(
+        f"SELECT count() FROM {CH_DATABASE}.superset_flat_analytics"
+    )
     print(f"Общее количество строк таблицы: {count_res:,}")
 
     client.close()
+
 
 if __name__ == '__main__':
     run_etl()
